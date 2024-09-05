5e464d73d8dc9fc927414d7421d77a78e10409ab
simxnet <simxnet@envs.net>
2024-09-06 00:47:14 +0200

    fix: wrong method

--- akashi-esi/src/job.rs
+++ akashi-esi/src/job.rs
@@ -46,7 +46,7 @@ impl SicJob {
         let temp_dir = temp_dir();
 
         #[cfg(target_os = "linux")]
-        let temp_dir = TempDir::new("sic").unwrap().path().into_path_buf();
+        let temp_dir = TempDir::new("sic").unwrap().path().to_path_buf();
 
         let file_path = temp_dir.join(&name);
 
